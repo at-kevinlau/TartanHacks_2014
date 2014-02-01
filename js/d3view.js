fe130aefa8e41d5b39641bd298a1cc77a8b2e5fe
Charles Chong <CC.Charles.Chong@gmail.com>
2014-02-01 04:43:35 -0500

    added arrows to links

--- js/d3view.js
+++ js/d3view.js
@@ -1,29 +1,6 @@
-var initNodes = [{
-  x: 100,
-  y: 200,
-  fixed: true,
-  courseId: "15112"
-},{
-  x: 300,
-  y: 200,
-  courseId: "15122"
-},{
-  x: 50,
-  y: 200,
-  courseId: "15213"
-},{
-  x: 50,
-  y: 400,
-  courseId: "15410"
-}]
+var initNodes = [{"courseId":"18051","treeType":"OR","nodeId":0},{"courseId":"","treeType":"OR","nodeId":1},{"courseId":"18100","treeType":"OR","nodeId":3},{"courseId":"","treeType":"OR","nodeId":4},{"courseId":"18202","treeType":"OR","nodeId":6},{"courseId":"18213","treeType":"OR","nodeId":11},{"courseId":"18220","treeType":"OR","nodeId":14},{"courseId":"18232","treeType":"OR","nodeId":16},{"courseId":"","treeType":"OR","nodeId":17},{"courseId":"18240","treeType":"OR","nodeId":19},{"courseId":"18290","treeType":"OR","nodeId":21},{"courseId":"18310","treeType":"OR","nodeId":22},{"courseId":"18332","treeType":"OR","nodeId":24},{"courseId":"","treeType":"OR","nodeId":25},{"courseId":"18340","treeType":"OR","nodeId":27},{"courseId":"18345","treeType":"AND","nodeId":29},{"courseId":"","treeType":"OR","nodeId":30},{"courseId":"","treeType":"OR","nodeId":34},{"courseId":"","treeType":"OR","nodeId":35},{"courseId":"18348","treeType":"AND","nodeId":37},{"courseId":"18390","treeType":"OR","nodeId":39},{"courseId":"","treeType":"OR","nodeId":40},{"courseId":"18402","treeType":"OR","nodeId":42},{"courseId":"18411","treeType":"AND","nodeId":45},{"courseId":"18418","treeType":"OR","nodeId":50},{"courseId":"18421","treeType":"AND","nodeId":52},{"courseId":"18422","treeType":"AND","nodeId":55},{"courseId":"18432","treeType":"OR","nodeId":57},{"courseId":"","treeType":"OR","nodeId":58},{"courseId":"18447","treeType":"AND","nodeId":60},{"courseId":"","treeType":"OR","nodeId":61},{"courseId":"","treeType":"OR","nodeId":62},{"courseId":"","treeType":"OR","nodeId":65},{"courseId":"","treeType":"OR","nodeId":68},{"courseId":"18474","treeType":"AND","nodeId":70},{"courseId":"","treeType":"OR","nodeId":71},{"courseId":"","treeType":"OR","nodeId":72},{"courseId":"","treeType":"OR","nodeId":75},{"courseId":"18482","treeType":"OR","nodeId":77},{"courseId":"18491","treeType":"OR","nodeId":80},{"courseId":"18510","treeType":"OR","nodeId":82},{"courseId":"","treeType":"AND","nodeId":83},{"courseId":"","treeType":"AND","nodeId":84},{"courseId":"","treeType":"AND","nodeId":85},{"courseId":"","treeType":"AND","nodeId":86},{"courseId":"","treeType":"AND","nodeId":87},{"courseId":"","treeType":"AND","nodeId":88},{"courseId":"","treeType":"AND","nodeId":89},{"courseId":"18540","treeType":"AND","nodeId":91},{"courseId":"","treeType":"OR","nodeId":92},{"courseId":"","treeType":"OR","nodeId":93},{"courseId":"","treeType":"OR","nodeId":94},{"courseId":"18549","treeType":"AND","nodeId":96},{"courseId":"","treeType":"OR","nodeId":97},{"courseId":"","treeType":"OR","nodeId":99},{"courseId":"","treeType":"OR","nodeId":100},{"courseId":"18551","treeType":"AND","nodeId":102},{"courseId":"","treeType":"OR","nodeId":103},{"courseId":"","treeType":"OR","nodeId":104},{"courseId":"","treeType":"OR","nodeId":106},{"courseId":"18578","treeType":"OR","nodeId":108},{"courseId":"","treeType":"AND","nodeId":109},{"courseId":"","treeType":"AND","nodeId":110},{"courseId":"","treeType":"AND","nodeId":112},{"courseId":"","treeType":"AND","nodeId":113},{"courseId":"","treeType":"AND","nodeId":114},{"courseId":"","treeType":"AND","nodeId":115},{"courseId":"","treeType":"AND","nodeId":116},{"courseId":"","treeType":"OR","nodeId":117},{"courseId":"18601","treeType":"OR","nodeId":119},{"courseId":"","treeType":"OR","nodeId":120},{"courseId":"18603","treeType":"OR","nodeId":122},{"courseId":"","treeType":"OR","nodeId":123},{"courseId":"18610","treeType":"OR","nodeId":125},{"courseId":"18615","treeType":"OR","nodeId":127},{"courseId":"18625","treeType":"OR","nodeId":129},{"courseId":"","treeType":"OR","nodeId":130},{"courseId":"18637","treeType":"OR","nodeId":132},{"courseId":"","treeType":"OR","nodeId":133},{"courseId":"18640","treeType":"OR","nodeId":135},{"courseId":"18641","treeType":"OR","nodeId":137},{"courseId":"","treeType":"OR","nodeId":138},{"courseId":"18645","treeType":"OR","nodeId":140},{"courseId":"","treeType":"OR","nodeId":141},{"courseId":"18698","treeType":"OR","nodeId":143},{"courseId":"","treeType":"OR","nodeId":144},{"courseId":"18721","treeType":"OR","nodeId":146},{"courseId":"18731","treeType":"OR","nodeId":149},{"courseId":"18732","treeType":"OR","nodeId":153},{"courseId":"18733","treeType":"OR","nodeId":155},{"courseId":"18739","treeType":"OR","nodeId":157},{"courseId":"18745","treeType":"OR","nodeId":159},{"courseId":"","treeType":"AND","nodeId":160},{"courseId":"","treeType":"AND","nodeId":161},{"courseId":"","treeType":"AND","nodeId":163},{"courseId":"","treeType":"AND","nodeId":164},{"courseId":"","treeType":"AND","nodeId":165},{"courseId":"","treeType":"AND","nodeId":166},{"courseId":"","treeType":"AND","nodeId":167},{"courseId":"18746","treeType":"OR","nodeId":169},{"courseId":"18748","treeType":"OR","nodeId":171},{"courseId":"18752","treeType":"OR","nodeId":173},{"courseId":"18753","treeType":"OR","nodeId":176},{"courseId":"18759","treeType":"AND","nodeId":178},{"courseId":"","treeType":"OR","nodeId":179},{"courseId":"","treeType":"OR","nodeId":181},{"courseId":"","treeType":"OR","nodeId":182},{"courseId":"18760","treeType":"AND","nodeId":184},{"courseId":"18764","treeType":"OR","nodeId":187},{"courseId":"18765","treeType":"AND","nodeId":189},{"courseId":"","treeType":"OR","nodeId":190},{"courseId":"","treeType":"OR","nodeId":191},{"courseId":"","treeType":"OR","nodeId":192},{"courseId":"","treeType":"OR","nodeId":193},{"courseId":"18791","treeType":"OR","nodeId":195},{"courseId":"","treeType":"OR","nodeId":196},{"courseId":"18795","treeType":"OR","nodeId":198},{"courseId":"18799","treeType":"OR","nodeId":200},{"courseId":"18819","treeType":"OR","nodeId":202},{"courseId":"18842","treeType":"AND","nodeId":204},{"courseId":"","treeType":"OR","nodeId":205},{"courseId":"","treeType":"OR","nodeId":209},{"courseId":"","treeType":"OR","nodeId":212},{"courseId":"18843","treeType":"OR","nodeId":214},{"courseId":"18845","treeType":"OR","nodeId":216},{"courseId":"18869","treeType":"OR","nodeId":218},{"courseId":"","treeType":"OR","nodeId":219},{"courseId":"18875","treeType":"OR","nodeId":221},{"courseId":"","treeType":"OR","nodeId":222},{"courseId":"18879","treeType":"OR","nodeId":224},{"courseId":"18902","treeType":"OR","nodeId":228},{"courseId":"","treeType":"OR","nodeId":229},{"courseId":"18980","treeType":"OR","nodeId":231},{"courseId":"","treeType":"OR","nodeId":232},{"courseId":"18984","treeType":"OR","nodeId":234},{"courseId":"","treeType":"OR","nodeId":235},{"courseId":"18990","treeType":"OR","nodeId":237},{"courseId":"","treeType":"OR","nodeId":238},{"courseId":"18992","treeType":"OR","nodeId":240},{"courseId":"","treeType":"OR","nodeId":241},{"courseId":"18994","treeType":"OR","nodeId":243},{"courseId":"","treeType":"OR","nodeId":244},{"courseId":"18995","treeType":"OR","nodeId":246},{"courseId":"","treeType":"OR","nodeId":247}]
 
-var initLinks = [{
-  source:0, target:1
-},{
-  source:1, target:2
-},{
-  source:2, target:3
-}]
+var initLinks = [{"source":0,"target":1},{"source":0,"target":2},{"source":3,"target":4},{"source":3,"target":5},{"source":6,"target":7},{"source":6,"target":8},{"source":6,"target":9},{"source":6,"target":10},{"source":11,"target":12},{"source":11,"target":13},{"source":14,"target":3},{"source":14,"target":6},{"source":14,"target":15},{"source":16,"target":17},{"source":16,"target":18},{"source":19,"target":3},{"source":19,"target":20},{"source":21,"target":3},{"source":21,"target":6},{"source":22,"target":14},{"source":22,"target":23},{"source":24,"target":25},{"source":24,"target":26},{"source":27,"target":19},{"source":27,"target":28},{"source":30,"target":31},{"source":30,"target":32},{"source":30,"target":33},{"source":29,"target":30},{"source":34,"target":11},{"source":29,"target":34},{"source":29,"target":35},{"source":29,"target":36},{"source":37,"target":19},{"source":37,"target":11},{"source":37,"target":38},{"source":39,"target":40},{"source":39,"target":41},{"source":42,"target":43},{"source":42,"target":44},{"source":45,"target":46},{"source":45,"target":5},{"source":45,"target":7},{"source":45,"target":47},{"source":45,"target":48},{"source":45,"target":49},{"source":50,"target":14},{"source":50,"target":51},{"source":52,"target":21},{"source":52,"target":53},{"source":52,"target":54},{"source":55,"target":19},{"source":55,"target":53},{"source":55,"target":56},{"source":57,"target":58},{"source":57,"target":59},{"source":61,"target":19},{"source":60,"target":61},{"source":62,"target":63},{"source":62,"target":11},{"source":62,"target":64},{"source":60,"target":62},{"source":65,"target":27},{"source":65,"target":66},{"source":65,"target":37},{"source":65,"target":67},{"source":65,"target":53},{"source":60,"target":65},{"source":60,"target":68},{"source":60,"target":69},{"source":71,"target":63},{"source":71,"target":64},{"source":70,"target":71},{"source":72,"target":73},{"source":72,"target":74},{"source":70,"target":72},{"source":70,"target":75},{"source":70,"target":76},{"source":77,"target":78},{"source":77,"target":79},{"source":80,"target":21},{"source":80,"target":81},{"source":83,"target":43},{"source":83,"target":53},{"source":82,"target":83},{"source":84,"target":43},{"source":84,"target":80},{"source":82,"target":84},{"source":85,"target":22},{"source":85,"target":53},{"source":82,"target":85},{"source":86,"target":22},{"source":86,"target":80},{"source":82,"target":86},{"source":87,"target":80},{"source":87,"target":53},{"source":82,"target":87},{"source":88,"target":43},{"source":88,"target":52},{"source":82,"target":88},{"source":89,"target":22},{"source":89,"target":52},{"source":82,"target":89},{"source":82,"target":90},{"source":92,"target":53},{"source":92,"target":74},{"source":92,"target":80},{"source":91,"target":92},{"source":93,"target":27},{"source":93,"target":66},{"source":93,"target":37},{"source":93,"target":67},{"source":91,"target":93},{"source":91,"target":94},{"source":91,"target":95},{"source":97,"target":53},{"source":97,"target":74},{"source":97,"target":80},{"source":97,"target":60},{"source":97,"target":98},{"source":96,"target":97},{"source":99,"target":37},{"source":99,"target":67},{"source":96,"target":99},{"source":96,"target":100},{"source":96,"target":101},{"source":103,"target":80},{"source":102,"target":103},{"source":104,"target":74},{"source":104,"target":105},{"source":104,"target":37},{"source":104,"target":67},{"source":102,"target":104},{"source":102,"target":106},{"source":102,"target":107},{"source":109,"target":37},{"source":109,"target":53},{"source":108,"target":109},{"source":110,"target":37},{"source":110,"target":111},{"source":108,"target":110},{"source":112,"target":74},{"source":112,"target":37},{"source":108,"target":112},{"source":113,"target":67},{"source":113,"target":74},{"source":108,"target":113},{"source":114,"target":67},{"source":114,"target":111},{"source":108,"target":114},{"source":115,"target":67},{"source":115,"target":53},{"source":108,"target":115},{"source":116,"target":74},{"source":116,"target":53},{"source":108,"target":116},{"source":108,"target":117},{"source":108,"target":118},{"source":119,"target":120}]
 
 var width = window.innerWidth,
     height = window.innerHeight,
@@ -38,6 +15,28 @@ var outer = d3.select("#chart")
     .attr("width", width)
     .attr("height", height)
     .attr("pointer-events", "all");
+
+outer.append('svg:defs').append('svg:marker')
+    .attr('id', 'end-arrow')
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 6)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
+    .attr('orient', 'auto')
+  .append('svg:path')
+    .attr('d', 'M0,-5L10,0L0,5')
+    .attr('fill', '#000');
+
+outer.append('svg:defs').append('svg:marker')
+    .attr('id', 'start-arrow')
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 4)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
+    .attr('orient', 'auto')
+  .append('svg:path')
+    .attr('d', 'M10,-5L0,0L10,5')
+    .attr('fill', '#000');
 
 var vis = outer
   .append('svg:g')
@@ -58,7 +57,7 @@ var force = d3.layout.force()
     .linkDistance(10)
     .linkStrength(1)
     .charge(-3000)
-    .gravity(0)
+    .gravity(1)
     .on("tick", tick);
 
 // get layout properties
@@ -66,8 +65,9 @@ var nodes = force.nodes(),
     links = force.links();
 
 var link = vis.selectAll(".link").data(links)
-    .enter().insert("line", ".node")
-    .attr("class", "link");
+    .enter().insert("svg:path")
+    .attr("class", "link")
+    .style('marker-end', 'url(#end-arrow)');
 
 var node = vis.selectAll(".node").data(nodes)
     .enter().append("g")
@@ -93,7 +93,21 @@ function tick() {
   link.attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+      .attr("y2", function(d) { return d.target.y; })
+      .attr('d', function(d) {
+        var deltaX = d.target.x - d.source.x,
+        deltaY = d.target.y - d.source.y,
+        dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+        normX = deltaX / dist,
+        normY = deltaY / dist,
+        sourcePadding = 5,
+        targetPadding = 5,
+        sourceX = d.source.x + (sourcePadding * normX),
+        sourceY = d.source.y + (sourcePadding * normY),
+        targetX = d.target.x - (targetPadding * normX),
+        targetY = d.target.y - (targetPadding * normY);
+        return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
+      });
       
   node.attr("transform",
       function(d,i) {return "translate(" + d.x + ", " + d.y + ")"});
